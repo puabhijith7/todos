e8c8d7da3b921dce1132a47ca1b1b21773f298e8
puabhijith7 <235814@ust-global.com>
2022-10-12 17:30:43 +0530

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular TUDOS';
+  s:any
+  a :any
+  b:any
+   p=0;
+          fun(t:any)
+         {
+            // d(this.p+2);
+              this.a=t;
+            // this.b=d;
+             this.s=this.a
+             this.p++;
+             //this.disp();
+            
+         
+         }
+     /*      disp()
+         {
+         document.getElementById("ans").innerHTML=s;
+         }
+         function d(y)
+         {
+             var e=`Enter Customer `+y+` details :`;
+             document.getElementById("z").innerHTML=e;
+         }
+  */
 }
