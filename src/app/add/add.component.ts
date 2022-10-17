6082c6248ddd54a6198c2c2e31db5faa78e64302
puabhijith7 <235814@ust-global.com>
2022-10-17 15:35:37 +0530

    8

--- src/app/add/add.component.ts
+++ src/app/add/add.component.ts
@@ -12,8 +12,10 @@ export class AddComponent implements OnInit {
   a:any[] =[]
   h:any
   j:any
+  q:any
+  i:any
   b:any[] =[]
-          fun(t:any,d:any)
+        fun(t:any,d:any)
          {             
               if((t=='') && (d=='')) 
               {
@@ -22,13 +24,17 @@ export class AddComponent implements OnInit {
               else{ 
               this.h="Event Name"
               this.j="Date"
+              this.q="Sl no"
               this.a.push({t})
-              this.b.push({d})
+              this.b.push({d})           
               }
          }
          delete1(i:any){
           this.a.splice(i,1)
           this.b.splice(i,1)
+          this.h=''
+          this.j=''
+          this.q=''
          } 
   ngOnInit(): void {    
   }
